0c9e254f132a1366609296686e5c44d53aa35d0d
jc578271 <57795010+jc578271@users.noreply.github.com>
2025-06-16 01:02:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +e
+set -x
 # Install dependencies
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install akmod-nvidia
