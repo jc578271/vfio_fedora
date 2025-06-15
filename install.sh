2a3a66ea2a67c36ef205e8ff6ae4214c20220a4d
jc578271 <57795010+jc578271@users.noreply.github.com>
2025-06-15 23:28:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set +e
 # Install dependencies
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install akmod-nvidia
@@ -25,22 +25,5 @@ sudo dracut -f --kver `uname -r`
 # Install System76 packages
 sudo dnf copr enable szydell/system76
 sudo dnf install system76-dkms system76-power system76-driver system76-firmware firmware-manager system76-io-dkms system76-acpi-dkms
-sudo systemctl enable com.system76.PowerDaemon.service system76-power-wake system76-firmware-daemon --now
-sudo systemctl enable --user com.system76.FirmwareManager.Notify.timer
-
-# Attached, deattached nvidia scripts
-sudo mkdir -p /etc/libvirt/hooks
-sudo wget 'https://raw.githubusercontent.com/PassthroughPOST/VFIO-Tools/master/libvirt_hooks/qemu' \
-     -O /etc/libvirt/hooks/qemu
-sudo chmod +x /etc/libvirt/hooks/qemu
-
-sudo systemctl restart libvirtd
-
-# Enable SSH
-sudo systemctl enable sshd
-sudo systemctl start sshd
-
-# Add your current user to the group “libvirt”.
-sudo usermod -G libvirt -a $USER
 
 echo "Reboot to apply changes"
