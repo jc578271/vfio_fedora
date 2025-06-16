cca213cbacdba3ef35be0419b2de71bd0d69f477
jc578271 <57795010+jc578271@users.noreply.github.com>
2025-06-17 02:56:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -x
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install akmod-nvidia
 sudo dnf install xorg-x11-drv-nvidia-cuda
+sudo dnf install kernel-devel-$(uname -r)
 sudo akmods --force
 modinfo -F version nvidia
 
